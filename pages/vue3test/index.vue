5ebe6a0c4e5cf3ef85ed19694fa150b3cbc24493
yy <yy@sweettracker.co.kr>
2023-09-06 19:04:05 +0900

    test sendbox 업로드

---
+++ pages/vue3test/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+  const age = ref(17)
+  const canVote = computed(() => {
+      return age.value >= 18 ? '가능' : '불가능'
+  })
+  const canVoteFunction = () => {
+    return age.value >= 18 ? '가능' : '불가능'
+  }
+  console.log(age, canVote)
+</script>
+
+<template>
+  <div>사용자 정보</div>
+  <ul>
+    <li>나이: {{ age }}</li>
+    <li>투표가 가능한가요?: {{ age >= 18? '가능' : '불가능' }}</li>
+    <li>투표가 가능한가요?: {{ canVote }}</li>
+    <li>투표가 가능한가요?: {{ canVoteFunction }}</li>
+  </ul>
+
+  <v-btn @click="age--">나이 -</v-btn>
+  <v-btn @click="age++">나이 +</v-btn>
+</template>
+
+<style scoped>
+
+</style>
