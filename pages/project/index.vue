d33b0ec5eebcc261f66abc7ab366ffcc4449f001
yy <yy@sweettracker.co.kr>
2023-09-18 17:27:32 +0900

    project 부분 타임라인 기본 setting

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -1,38 +1,101 @@
 <script setup>
 const cards = ['포트폴리오 1', '포트폴리오 2']
+const items = ref([
+  {
+    color: '#41B883',
+    icon: 'mdi-nuxt',
+    name: '비즈엠',
+    linkKey: true,
+    linkURL: 'http://dev-bizm-renewal.bizmsg.kr/'
+  },
+  {
+    color: '#41B883',
+    icon: 'mdi-nuxt',
+    name: '스MART',
+    linkKey: true,
+    linkURL: 'http://14.129.18.228/'
+  },
+  {
+    color: '#41B883',
+    icon: 'mdi-nuxt',
+    name: '스윗트래커 공식 페이지',
+    linkKey: true,
+    linkURL: 'http://www.sweettracker.co.kr/'
+  },
+  {
+    color: '#41B883',
+    icon: 'mdi-nuxt',
+    name: '11번가 배송조회 추적 관리',
+    linkKey: true,
+    linkURL: 'http://11st-admin.sweettracker.net/'
+  },
+  {
+    color: '#41B883',
+    icon: 'mdi-nuxt',
+    name: '스마트택배 API',
+    linkKey: true,
+    linkURL: 'https://tracking.sweettracker.co.kr/'
+  },
+  {
+    color: '#f44336',
+    icon: 'mdi-angularjs',
+    name: '악성 파일 및 메일 탐지 시스템',
+    linkKey: false
+  },
+  {
+    color: '#f44336',
+    icon: 'mdi-angularjs',
+    name: 'LogK',
+    linkKey: false
+  },
+  {
+    color: '#0769AD',
+    icon: 'mdi-jquery',
+    name: 'ADC Smart',
+    linkKey: false
+  },
+])
+const linkPage = (linkURL) => {
+  window.open(linkURL, '_blank')
+}
 </script>
 
 <template>
   <v-row>
-    <v-col
-        v-for="card in cards"
-        :key="card"
-        cols="12"
-    >
-      <v-card>
-        <v-list lines="two">
-          <v-list-subheader :title="card"></v-list-subheader>
+    <div style="width: 800px" class="mx-auto">
+      <v-timeline align="center">
+        <v-timeline-item
+            v-for="(item, i) in items"
+            :key="i"
+            :dot-color="item.color"
+            size="default"
+            :icon="item.icon"
+            icon-color="#ffffff"
+        >
+          <v-card class="bg-white">
+            <v-card-title>
+              <div>{{ item.name }}</div>
+            </v-card-title>
+            <v-card-text class="bg-white text--primarym mt-4">
+              <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod
+                convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an
+                salutandi sententiae.</p>
+              <v-btn
+                  v-if="item.linkKey"
+                  icon="mdi-link"
+                  class="my-4"
+                  color="grey"
+                  variant="outlined"
+                  density="comfortable"
+                  @click="linkPage(item.linkURL)"
+              >
 
-          <template v-for="n in 6" :key="n">
-            <v-list-item>
-              <template v-slot:prepend>
-                <v-avatar color="grey-darken-1"></v-avatar>
-              </template>
-
-              <v-list-item-title :title="`Message ${n}`"></v-list-item-title>
-
-              <v-list-item-subtitle title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique"></v-list-item-subtitle>
-            </v-list-item>
-
-            <v-divider
-                v-if="n !== 6"
-                :key="`divider-${n}`"
-                inset
-            ></v-divider>
-          </template>
-        </v-list>
-      </v-card>
-    </v-col>
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </div>
   </v-row>
 </template>
 
