e8688ca18c64b1e1d35a730073b8d94b27697bfb
yy <yy@sweettracker.co.kr>
2023-09-20 19:16:21 +0900

    summary 내용 추가

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -51,17 +51,27 @@ const items = ref([
     name: '스마트택배 API',
     linkKey: true,
     linkURL: 'https://tracking.sweettracker.co.kr/',
-    summaryDesc: '프로젝트 간략 설명',
+    summaryDesc: [
+      '트래픽 내 파일 및 메일 첨부파일의 악성여부를 필터링하고 모니터링 하는 대시보드 솔루션 개발',
+      '대형 전광판부터 개인 PC 및 모바일까지 소화하는 반응형 개발을 위해 Bootstarp 커스터마이징',
+      '웹 소켓을 이용하여 실시간 데이터 분석 현황판 구현',
+      '기획 참여, FE 단독 개발 및 유지보수, 국가 정보기관 납품'
+    ],
     taskDesc: '내가 맡은 업무',
     techDesc: 'Vue, Nuxt.js, AngularJs, Bootstrap, Jenkins'
   },
   {
     color: '#f44336',
     icon: 'mdi-angularjs',
-    name: '악성 파일 및 메일 탐지 시스템',
+    name: '악성파일 탐지 시스템',
     linkKey: false,
-    summaryDesc: '프로젝트 간략 설명',
-    taskDesc: 'FE 기획참여,',
+    summaryDesc: [
+      '트래픽 내 파일 및 메일 첨부파일의 악성여부를 필터링하고 모니터링 하는 대시보드 솔루션 개발',
+      '대형 전광판부터 개인 PC 및 모바일까지 소화하는 반응형 개발을 위해 Bootstarp 커스터마이징',
+      '웹 소켓을 이용하여 실시간 데이터 분석 현황판 구현',
+      '기획 참여, FE 단독 개발 및 유지보수, 국가 정보기관 납품'
+    ],
+    taskDesc: '데이터 시각화, 반응형, 실시간 데이터',
     techDesc: 'AngularJs, Bootstrap'
   },
   {
@@ -88,7 +98,7 @@ const items = ref([
       '모니터링 모듈들을 한 페이지에 카드 형식으로 배치하고 저장하여 사용할 수 있는 커스터마이징 대시보드를 서드파티 없이 단독 개발 세일링 포인트에 적극 활용',
       '수출형 모델을 위해 다국어 모듈 및 빌드 작업.'
     ],
-    taskDesc: '리뉴얼, 디자인 개편, 커스터마이징 대시보드 개발, amChart 리팩토링, 국가별 다국어 빌드, 데이터 시각화',
+    taskDesc: '리뉴얼, 커스터마이징 대시보드 개발, amChart 리팩토링, 국가별 다국어 빌드, 데이터 시각화',
     techDesc: 'Jquery, FreeMarker'
   }
 ])
