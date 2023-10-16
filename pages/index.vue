4ea6fb5b5288c53cdd5e0859077494749fa3daa2
yy <yy@sweettracker.co.kr>
2023-10-16 12:20:56 +0900

    사진 및 contact 부분 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import LeftNav from "~/components/nav/leftNav.vue";
 
+const yhun = ref('/yhun.png')
 const cards = ['포트폴리오 1', '포트폴리오 2']
 const mainContainerStyle = ref({
   width: '960px'
@@ -21,138 +22,148 @@ const experienceDesc = ref(
 </script>
 <template>
   <div class="mx-auto" :style="mainContainerStyle">
-  <v-row>
-    <v-col cols="12" md="6" class="text-center">ddd</v-col>
-    <v-col cols="12" md="6">
-      <div class="text-h5">Intro</div>
-      <v-textarea
-          class="pt-0 mb-6"
-          hide-details
-          no-resize
-          readonly
-          variant="plain"
-          rows="5"
-        v-model="IntroDesc"
-      ></v-textarea>
-      <v-divider></v-divider>
-      <div class="text-h5 mt-6">experience</div>
-      <v-textarea
-          class="pt-0 mb-6"
-          hide-details
-          no-resize
-          readonly
-          variant="plain"
-          rows="14"
-          v-model="experienceDesc"
-      ></v-textarea>
-      <v-divider></v-divider>
-      <div class="text-h5 mb-6"></div>
-      <div class="text-h5 my-6">Biography</div>
-      <div class="mb-6">
-        <v-row>
-          <v-col cols="4">
-            <div>2017.01 ~ 2023.11</div>
+    <v-row>
+      <v-col cols="12" md="6" class="text-center">
+        <div class="mx-auto" style="width: 300px">
+          <v-avatar :image="yhun" size="250"></v-avatar>
+          <div class="text-left">
+            <div class="my-6 text-h5">Contact</div>
+            <div>Phone. 010-9936-0470</div>
+            <div>Email. doubleyhun@gmail.com</div>
+            <div>Github. <a href="https://github.com/doubleyhun">https://github.com/doubleyhun</a></div>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" md="6">
+        <div class="text-h5">Intro</div>
+        <v-textarea
+            class="pt-0 mb-6"
+            hide-details
+            no-resize
+            readonly
+            variant="plain"
+            rows="5"
+            v-model="IntroDesc"
+        ></v-textarea>
+        <v-divider></v-divider>
+        <div class="text-h5 mt-6">experience</div>
+        <v-textarea
+            class="pt-0 mb-6"
+            hide-details
+            no-resize
+            readonly
+            variant="plain"
+            rows="14"
+            v-model="experienceDesc"
+        ></v-textarea>
+        <v-divider></v-divider>
+        <div class="text-h5 mb-6"></div>
+        <div class="text-h5 my-6">Biography</div>
+        <div class="mb-6">
+          <v-row>
+            <v-col cols="4">
+              <div>2017.01 ~ 2023.11</div>
+            </v-col>
+            <v-col cols="8">
+              <div>써머스플랫폼 스윗트래커CIC UXD팀 FE 개발자</div>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="4">
+              <div>2012.08 ~ 2016.11</div>
+            </v-col>
+            <v-col cols="8">
+              <div>오픈베이스 기업 부설 연구소 소속 FE 개발자</div>
+            </v-col>
+          </v-row>
+        </div>
+        <v-divider></v-divider>
+        <div class="text-h5 my-6">Skills</div>
+        <v-row class="mb-6">
+          <v-col cols="12" md="6">
+            <v-alert
+                class="my-2"
+                border="start"
+                variant="tonal"
+                color="#FFD600"
+                theme="dark"
+                icon="mdi-language-javascript"
+                prominent
+                title="Javascript"
+            >
+            </v-alert>
+            <v-alert
+                class="my-2"
+                border="start"
+                variant="tonal"
+                color="#81C784"
+                theme="dark"
+                icon="mdi-vuejs"
+                prominent
+                title="Vue"
+            >
+            </v-alert>
+            <v-alert
+                class="my-2"
+                border="start"
+                variant="tonal"
+                color="#41B883"
+                theme="dark"
+                icon="mdi-nuxt"
+                prominent
+                title="Nuxt"
+            >
+            </v-alert>
           </v-col>
-          <v-col cols="8">
-            <div>써머스플랫폼 스윗트래커CIC UXD팀 FE 개발자</div>
+          <v-col cols="12" md="6">
+            <v-alert
+                class="my-2"
+                border="start"
+                variant="tonal"
+                color="blue"
+                theme="dark"
+                icon="$vuetify"
+                prominent
+                title="Vuetify"
+            >
+            </v-alert>
+            <v-alert
+                class="my-2"
+                border="start"
+                variant="tonal"
+                color="#f14a29"
+                theme="dark"
+                icon="mdi-language-html5"
+                prominent
+                title="HTML/CSS"
+            >
+            </v-alert>
+            <v-alert
+                class="my-2"
+                border="start"
+                variant="tonal"
+                color="#BF4080"
+                theme="dark"
+                icon="mdi-sass"
+                prominent
+                title="SASS"
+            >
+            </v-alert>
           </v-col>
         </v-row>
-        <v-row>
-          <v-col cols="4">
-            <div>2012.08 ~ 2016.11</div>
-          </v-col>
-          <v-col cols="8">
-            <div>오픈베이스 기업 부설 연구소 소속 FE 개발자</div>
-          </v-col>
-        </v-row>
-      </div>
-      <v-divider></v-divider>
-      <div class="text-h5 my-6">Skills</div>
-      <v-row class="mb-6">
-        <v-col cols="12" md="6">
-          <v-alert
-              class="my-2"
-              border="start"
-              variant="tonal"
-              color="#FFD600"
-              theme="dark"
-              icon="mdi-language-javascript"
-              prominent
-              title="Javascript"
-          >
-          </v-alert>
-          <v-alert
-              class="my-2"
-              border="start"
-              variant="tonal"
-              color="#81C784"
-              theme="dark"
-              icon="mdi-vuejs"
-              prominent
-              title="Vue"
-          >
-          </v-alert>
-          <v-alert
-              class="my-2"
-              border="start"
-              variant="tonal"
-              color="#41B883"
-              theme="dark"
-              icon="mdi-nuxt"
-              prominent
-              title="Nuxt"
-          >
-          </v-alert>
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-alert
-              class="my-2"
-              border="start"
-              variant="tonal"
-              color="blue"
-              theme="dark"
-              icon="$vuetify"
-              prominent
-              title="Vuetify"
-          >
-          </v-alert>
-          <v-alert
-              class="my-2"
-              border="start"
-              variant="tonal"
-              color="#f14a29"
-              theme="dark"
-              icon="mdi-language-html5"
-              prominent
-              title="HTML/CSS"
-          >
-          </v-alert>
-          <v-alert
-              class="my-2"
-              border="start"
-              variant="tonal"
-              color="#BF4080"
-              theme="dark"
-              icon="mdi-sass"
-              prominent
-              title="SASS"
-          >
-          </v-alert>
-        </v-col>
-      </v-row>
-      <v-divider></v-divider>
-      <div class="text-h5 my-6">Education</div>
-      <div class="mb-6">
-        <v-row>
-          <v-col cols="4">
-            <div>2006.03 ~ 2012.08</div>
-          </v-col>
-          <v-col cols="8">
-            <div>한남대학교 멀티미디어 공학, 미디어영상학 학사</div>
-          </v-col>
-        </v-row>
-      </div>
-    </v-col>
-  </v-row>
+        <v-divider></v-divider>
+        <div class="text-h5 my-6">Education</div>
+        <div class="mb-6">
+          <v-row>
+            <v-col cols="4">
+              <div>2006.03 ~ 2012.08</div>
+            </v-col>
+            <v-col cols="8">
+              <div>한남대학교 멀티미디어 공학, 미디어영상학 학사</div>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
